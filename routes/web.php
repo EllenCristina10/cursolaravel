56fa8a116d2070ed085ab9a60eaf4a6363e61276
Ellen Cristina <ellecristina42@gmail.com>
2023-09-01 12:13:58 -0300

    Rotas, Controller, Migration e Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/empresa', function(){
-    return view('site/empresa');
-});
+Route::resource('produtos', ProdutoController::class);
